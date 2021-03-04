c186ba5354dd0a05936981c33328a6ddf8c54f8c
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2021-03-04 20:12:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,4 +10,13 @@ class School
     @roster
   end
   
+  def add_student(student_name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << student_name
+  end
+  
 end
